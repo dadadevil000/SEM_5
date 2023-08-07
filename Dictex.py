7fcefd272cf53759f1da9d2680aadc263423535b
Makarand Hinge <mmh2212002@gmail.com>
2023-08-07 10:23:06 +0530

    2

--- Dictex.py
+++ Dictex.py
@@ -1,3 +1,11 @@
 a ={1:"first_name",2:"last_name","age":20}
 print(a)
 print(a["age"])
+a["age"]=25
+print(a)
+x ={"coursel","coursel2","coursel3"}
+print(type(x))
+set1 ={"SITCOE"}
+print(set1)
+s2=set("SITCOE")
+print(s2)
